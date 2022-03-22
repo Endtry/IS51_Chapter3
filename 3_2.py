860805d839162437534061445b6566ee00fcd04b
endtry <lucas123phan@gmail.com>
2022-03-22 15:39:40 -0700

    adding more codes

---
+++ 3_2.py
@@ -0,0 +1,43 @@
+
+
+# grade = 90
+# condition = grade >=90
+# if not condition:
+#     # execute when true
+#     print("Your grade is A")
+# else:
+#     # execute when False
+#     print("Your grade is not A")
+
+
+
+# num1 = eval(input("Enter the first number: "))
+# num2 = eval(input("Enter the second number: "))
+# try:
+#     print("1", isinstance(num1, str))
+#     print("2", isinstance(num2, str))
+#     if isinstance(num1, str) or isinstance(num2, str):
+#         print("Data type not allowed! Please specify a numeric value.")
+#     else:
+#         if num1 > num2:
+#             largerVal = num1
+#         else:
+#             largerVal = num2
+
+#         print("The larger value is " + str(largerVal))
+# except:
+#     print("Data not allowd!")
+
+# answer = eval(input("How many gallons does a ten-gallon hat hold? "))
+
+# if (.5 <= answer <= 1):
+#     print("Good, ", end="")
+# else:
+#     print("No, ", end="")
+# print("it holds about 3/4 of a gallon.")
+
+def is_greater_than10():
+    answer = eval(input("How many gallons does a ten-gallon hat hold? "))
+    if answer > 10:
+        return True
+    return False
