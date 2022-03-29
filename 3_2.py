e9d2fa0605e069967a7467ea4cd8acfd9cd9a37c
endtry <lucas123phan@gmail.com>
2022-03-29 15:24:28 -0700

    adding more codes

--- 3_2.py
+++ 3_2.py
@@ -10,7 +10,6 @@
 #     print("Your grade is not A")
 
 
-
 # num1 = eval(input("Enter the first number: "))
 # num2 = eval(input("Enter the second number: "))
 # try:
@@ -36,8 +35,120 @@
 #     print("No, ", end="")
 # print("it holds about 3/4 of a gallon.")
 
-def is_greater_than10():
-    answer = eval(input("How many gallons does a ten-gallon hat hold? "))
-    if answer > 10:
-        return True
-    return False
+# def is_greater_than10():
+#     answer = eval(input("How many gallons does a ten-gallon hat hold? "))
+#     if answer > 10:
+#         return True
+#     return False
+
+# first_num = eval(input("Enter first number: "))
+# second_num = eval(input("Enter second number: "))
+# third_num = eval(input("Enter third number: "))
+
+# max = first_num
+
+# if second_num > max:
+#     max = second_num
+# if third_num > max:
+#     max = third_num
+# print("The largest of the three number is " + str(max) + ".")
+
+# numbers = [1, 20, 90]
+
+# print(max(numbers))
+# numbers = []
+# first_num = eval(input("Enter first number: "))
+# numbers.append(first_num)
+# second_num = eval(input("Enter second number: "))
+# numbers.append(second_num)
+# third_num = eval(input("Enter third number: "))
+# numbers.append(third_num)
+# msg = "The largest of the three number is " + str(max(numbers)) + "."
+# print(msg)
+
+# color = input("Enter a color (BLUE or RED): ").upper()
+# mode = input("Enter a mode (STEADY or FLASHING): ").upper()
+
+# result = ""
+
+# if color == "BLUE":
+#     if mode == "STEADY":
+#         result = "Clear View"
+#     else: # mode is FLASHING
+#         result = "Cloud Due"
+# else: #Color is RED
+#     if mode == "STEADY":
+#         result = "Rain Ahead"
+#     else: # mode is FLASHING
+#         result = "Snow Instead"
+
+
+# costs = eval(input("Enter total cost: "))
+# revenue = eval(input("Enter total revenue: "))
+
+# if costs == revenue:
+#     result = "break Even."
+# else:
+#     if costs < revenue:
+#         profit = revenue - costs
+#         result = "Profit is ${0:,.2f}}".format(profit)
+#     else:
+#         loss = costs - revenue
+#         result = "Loss is ${0:,.2f}".format(loss)
+# print(result)
+
+# costs = eval(input("Enter total cost: "))
+# revenue = eval(input("Enter total revenue: "))
+
+# if costs == revenue:
+#     print("break Even.")
+# result =  "is ${0:,.2f}}".format(revenue - costs)
+# if (revenue - costs) < 0:
+#     print("Loss " + result)
+# print("Profit " + result)
+
+# num1 = eval(input("Enter the first number: "))
+# num2 = eval(input("Enter the second number: "))
+
+# if num1 > num2:
+#     print("The larger value is " + str(num1) + ".")
+# elif num2 > num1:
+#     print("The larger value is " + str(num2) + ".")
+# else:
+#     print("The two numbers are equal.")
+
+# gpa = eval(input("Enter your GPA: "))
+
+# if gpa >= 3.9:
+#     honors = " suma cum laude."
+# elif gpa >= 3.6:
+#     honors = " magna cum laude."
+# elif gpa >= 3.3:
+#     honors = " cum laude."
+# else:
+#     honors = "."
+# print("You graduated" + honors)
+
+
+# num1 = input("enter first number: ")
+# num2 = input("enter second number: ")
+
+# if num1.isdigit() and num2.isdigit():
+#     print("The sum is " + str(eval(num1) + eval(num2)) + ".")
+# elif not num1.isdigit():
+#     if not num2.isdigit():
+#         print("Neither entry was a proper number.")
+#     print("The first entry was not a proper number")
+# print("the second entry was not a proper number.")
+# if 0:
+#     print("This will not run...")
+# if 7:
+#     print("This will run...")
+# if []:
+#     print("This will not run...")
+# if [1, 2, 99]:
+#     print("This will run...")
+# if True:
+#     print("This will run...")
+# if False:
+#     print("This will not run...")
