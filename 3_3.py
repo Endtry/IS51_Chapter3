53f822e251fcdda7f95637518e0762766c4b4f37
endtry <lucas123phan@gmail.com>
2022-04-19 14:41:47 -0700

    finishing up chapter 3.3

--- 3_3.py
+++ 3_3.py
@@ -73,11 +73,40 @@
 # print("In ", str(i), " years you will have a million dollars.")
 
 
-list1 = []
-while True:
-    num = eval(input("Enter a nonnegative number: "))
-    if num == -1:
-        break
-    list1.append(num)
+# list1 = []
+# while True:
+#     num = eval(input("Enter a nonnegative number: "))
+#     if num == -1:
+#         break
+#     list1.append(num)
 
-print(list1)
+# print(list1)
+
+
+# print("Enter a number from the menu to obtain a fact")
+# print("about the United States or to exit the program.\n")
+
+# print("1. Capital")
+# print("2. National Bird")
+# print("3. National Flower")
+# print("4. Quit\n")
+
+
+# while True:
+#     num = int(input("Make a selection from the menu: "))
+#     if num == 1:
+#         print("Washington, DC is the capital of the United States.")
+#     elif num == 2:
+#         print("The American Bald Eagle is the national bird.")
+#     elif num == 3:
+#         print("The rose is the national flower.")
+#     elif num == 4: 
+#         break
+
+# Warning, do not uncomment
+# i = 0
+# while True:
+#     i += 1
+#     print("{} This is an infinate loop.".format(str(i)))
+
+
